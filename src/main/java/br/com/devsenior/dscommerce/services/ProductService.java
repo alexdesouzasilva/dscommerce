ad5e84bd0c6cbaf494f7b4ea654b1fe380e4e6ab
Alex <alexdesouzasilva94@gmail.com>
2024-06-26 05:38:31 -0300

    Listando produtos paginados

--- src/main/java/br/com/devsenior/dscommerce/services/ProductService.java
+++ src/main/java/br/com/devsenior/dscommerce/services/ProductService.java
@@ -1,8 +1,8 @@
 package br.com.devsenior.dscommerce.services;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +20,13 @@ public class ProductService {
     public ProductDTO findById(Long id) {
         Product product = repository.findById(id).get(); //.get(), pois é retornado um Optional.
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true) // lock de apenas leitura. Deixa mais rápido
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable); // Retorna Objeto Page
+        return result.map(x -> new ProductDTO(x)); // Converte Page<Product> para Page<ProductDTO>
+        // Como Page já é um stream, não precisamos convertê-lo usando stream()
     }
     
 }
