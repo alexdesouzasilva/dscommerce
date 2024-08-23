a6b0284d60fb74a69ecd40fc46dd42d6ed625489
Alex <alexdesouzasilva94@gmail.com>
2024-08-23 05:21:14 -0300

    Password and JWT grant

--- src/main/java/br/com/devsenior/dscommerce/controller/ProductController.java
+++ src/main/java/br/com/devsenior/dscommerce/controller/ProductController.java
@@ -5,12 +5,12 @@ import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,8 +20,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import br.com.devsenior.dscommerce.dto.ProductDTO;
 import br.com.devsenior.dscommerce.services.ProductService;
 import jakarta.validation.Valid;
-
-import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController // Respondendo pela web
